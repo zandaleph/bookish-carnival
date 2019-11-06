cf99621dda7bc8223ea5347fa93bb2da99b479b3
Zack Spencer <zspencer@Zacks-MacBook-Pro.local>
2019-11-06 10:33:59 -0800

    Add date, author, and lead to blog post template

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,24 +1,38 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import { css } from '@emotion/core';
+import ordinal from 'ordinal';
 import Layout from '../components/Layout';
+
 export default function BlogPost({ data }) {
   const post = data.markdownRemark;
+  const fm = post.frontmatter;
+  const date = `${fm.month} ${ordinal(parseInt(fm.day))}, ${fm.year}`;
   return (
     <Layout>
-      <div>
-        <h1>{post.frontmatter.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-      </div>
+      <h1>{fm.title}</h1>
+      <p>Posted {date} by Zack Spencer</p>
+      <p
+        css={css`
+          font-style: italic;
+        `}
+      >
+        {fm.lead}
+      </p>
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
     </Layout>
   );
 }
+
 export const query = graphql`
   query($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
       frontmatter {
         title
-        date
+        day: date(formatString: "DD")
+        month: date(formatString: "MMMM")
+        year: date(formatString: "YYYY")
         lead
       }
     }
