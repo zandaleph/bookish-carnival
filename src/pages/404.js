375e089055b5d6333088d838423b71035a514394
Zack Spencer <zspencer@Zacks-MacBook-Pro.local>
2019-11-09 12:40:53 -0800

    Match existing 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,8 +4,9 @@ import Layout from '../components/Layout';
 export default function NotFoundPage() {
   return (
     <Layout>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>
+        I&apos;m terribly sorry to tell you this, but an error has occurred.
+      </p>
     </Layout>
   );
 }
