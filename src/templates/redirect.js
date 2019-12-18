a7739c0fb54a0e2480283da3af7f5cf92b530f66
Zack Spencer <zspencer@Zacks-MacBook-Pro.local>
2019-12-18 09:27:56 -0800

    Remove unnecessary console.log from redirect

--- src/templates/redirect.js
+++ src/templates/redirect.js
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 import { navigate } from 'gatsby';
 
 export default function Redirect({ pageContext }) {
-  console.log(pageContext.slug);
   useEffect(() => navigate(pageContext.slug, { replace: true }));
   return null;
 }
