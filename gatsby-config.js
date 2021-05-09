66d95ead89afaa4c6105900aa3153a031873ce31
Zack Spellman <zandaleph@gmail.com>
2021-05-09 09:08:13 -0700

    Switch to npm, remove gatsby-plugin-sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,6 @@ module.exports = {
     "gatsby-plugin-emotion",
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
